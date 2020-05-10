8cb41b81b3981aa408534e71832a1b911e0f6f86
DaliaZH <Dalia_zhran@hotmail.com>
2020-05-11 01:51:21 +0200

    get key parkings from redis

--- controllers/parking.controller.js
+++ controllers/parking.controller.js
@@ -1,38 +1,79 @@
 var express = require("express");
 var router = express.Router();
 
-var redis = require('redis');
+var redis = require("redis");
 var client = redis.createClient(6379);
 
 // var client = require('../startup/db');
 // var client;
 
 router.get("/info", async (req, res) => {
-    try {
-      return res.status(200).send("Connected to Server Successfully");
-    } catch (error) {
-      return res.status(400).send(error.message);
-    }
+  try {
+    return res.status(200).send("Connected to Server Successfully");
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 });
 
 router.post("/set-key", (req, res) => {
-    let key = req.body.key;
-    client.get(key, function (error, result) {
-        if (error) {
-            console.log(error);
-            res.status(503).send({ error: "DB Error:(" });
-            throw error;
-        } else {
-            console.log('GET result ->' + result);
-            res.status(200).send({ value: result});
-        }
-    });
+  let key = req.body.key;
+  client.get(key, function(error, result) {
+    if (error) {
+      console.log(error);
+      res.status(503).send({ error: "DB Error:(" });
+      throw error;
+    } else {
+      console.log("GET result ->" + result);
+      res.status(200).send({ value: result });
+    }
+  });
 });
 
-// const setClient = (inClient) => { 
-//     client = inClient; 
-// }
+router.get("/a", (req, res) => {
+  var arr = [];
+  client.keys("park*", function(e, keys) {
+    if (e) {
+      console.log(e);
+      res.status(503).send({ error: "key Error:(" });
+      throw error;
+    }
+    client.mget(keys, function(err, values) {
+      if (err) {
+        console.log(err);
+        res.status(503).send({ error: "mget Error:(" });
+        throw error;
+      }
+      console.log(values);
+      res.status(200).send({ values: values, keys: keys });
+      //   callback(values);
+    });
+  });
+});
 
+// router.get("/a", (req, res) => {
+//   var arr = [];
+//   client.keys("*", function(e, keys) {
+//     if (e) {
+//       console.log(e);
+//       res.status(503).send({ error: "key Error:(" });
+//       throw error;
+//     }
+//     client.mget(keys, function(err, values) {
+//       if (err) {
+//         console.log(err);
+//         res.status(503).send({ error: "mget Error:(" });
+//         throw error;
+//       }
+//       console.log(values);
+//       res.status(200).send({ values: values, keys: keys });
+//       //   callback(values);
+//     });
+//   });
+// });
+
+// const setClient = (inClient) => {
+//     client = inClient;
+// }
 
 module.exports = router;
 // module.exports = {
