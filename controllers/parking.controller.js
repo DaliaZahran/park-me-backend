88571110b4ae252726590c780c837e2f2c3bbd5d
DaliaZH <Dalia_zhran@hotmail.com>
2020-05-11 10:47:22 +0200

    fixed redis get data

--- controllers/parking.controller.js
+++ controllers/parking.controller.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var redis = require("redis");
 var client = redis.createClient(6379);
 
+var async = require("async");
+
 // var client = require('../startup/db');
 // var client;
 
@@ -29,30 +31,9 @@ router.post("/set-key", (req, res) => {
   });
 });
 
-router.get("/a", (req, res) => {
-  var arr = [];
-  client.keys("park*", function(e, keys) {
-    if (e) {
-      console.log(e);
-      res.status(503).send({ error: "key Error:(" });
-      throw error;
-    }
-    client.mget(keys, function(err, values) {
-      if (err) {
-        console.log(err);
-        res.status(503).send({ error: "mget Error:(" });
-        throw error;
-      }
-      console.log(values);
-      res.status(200).send({ values: values, keys: keys });
-      //   callback(values);
-    });
-  });
-});
-
 // router.get("/a", (req, res) => {
 //   var arr = [];
-//   client.keys("*", function(e, keys) {
+//   client.keys("park*", function(e, keys) {
 //     if (e) {
 //       console.log(e);
 //       res.status(503).send({ error: "key Error:(" });
@@ -70,6 +51,46 @@ router.get("/a", (req, res) => {
 //     });
 //   });
 // });
+
+router.get("/a", (req, res) => {
+  // var arr = [];
+  // client.keys("*", function(e, keys) {
+  //   if (e) {
+  //     console.log(e);
+  //     res.status(503).send({ error: "key Error:(" });
+  //     throw error;
+  //   }
+  //   client.mget(keys, function(err, values) {
+  //     if (err) {
+  //       console.log(err);
+  //       res.status(503).send({ error: "mget Error:(" });
+  //       throw error;
+  //     }
+  //     console.log(values);
+  //     res.status(200).send({ values: values, keys: keys });
+  //     //   callback(values);
+  //   });
+  // });
+
+  client.keys("*", function(err, keys) {
+    if (err) return console.log(err);
+    if (keys) {
+      async.map(
+        keys,
+        function(key, cb) {
+          client.hgetall(key, function(error, value) {
+            if (error) return cb(error);
+            cb(null, value);
+          });
+        },
+        function(error, results) {
+          if (error) return console.log(error);
+          res.json({ values: results });
+        }
+      );
+    }
+  });
+});
 
 // const setClient = (inClient) => {
 //     client = inClient;
